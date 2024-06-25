23b470c6a72ce0affb44e27459a4f7c7bae231ce
tanmayysain <tanmaysain02@gmail.com>
2024-06-25 14:43:52 +0530

    Adding basic auth

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,36 @@
+import React, { useRef, useImperativeHandle } from "react";
+import classes from "./Input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activateInput = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activateInput,
+    };
+  });
+  
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
